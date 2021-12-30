555637d875d124c1bbf14f2cea66b537b11af931
mins1031 <urisegea@naver.com>
2021-12-30 23:39:19 +0900

    자동설정 관련 커밋

--- spring-boot-autoconfig/src/main/java/min/prac/HolomanConfiguration.java
+++ spring-boot-autoconfig/src/main/java/min/prac/HolomanConfiguration.java
@@ -1,5 +1,6 @@
 package min.prac;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +8,9 @@ import org.springframework.context.annotation.Configuration;
 public class HolomanConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
+//    @ConditionalOnMissingBean은 해당 어플리케이션이 다른 곳에 의존성으로 쓰일때 아래와 같은 bean을 직접 등록한다면
+    //아래의 Holoman빈은 등록 우선순위가 밀려나게 하는 역할을 한다
     public Holoman holoman() {
         Holoman holoman = new Holoman();
         holoman.setHowLong(5);
