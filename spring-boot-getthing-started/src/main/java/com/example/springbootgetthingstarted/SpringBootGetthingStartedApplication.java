582ccd11a53d43ba05dd6321cbd6a295905e2371
mins1031 <urisegea@naver.com>
2022-01-02 17:28:38 +0900

    tomcat servlet 코드

--- spring-boot-getthing-started/src/main/java/com/example/springbootgetthingstarted/SpringBootGetthingStartedApplication.java
+++ spring-boot-getthing-started/src/main/java/com/example/springbootgetthingstarted/SpringBootGetthingStartedApplication.java
@@ -1,11 +1,17 @@
 package com.example.springbootgetthingstarted;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @SpringBootApplication
 public class SpringBootGetthingStartedApplication {
@@ -13,11 +19,27 @@ public class SpringBootGetthingStartedApplication {
     public static void main(String[] args) throws LifecycleException {
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
-        tomcat.addContext("/", "/");
+        Context context = tomcat.addContext("/", "/");
+
+        HttpServlet servlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                PrintWriter printWriter = resp.getWriter();
+                printWriter.println("<html><head><title>");
+                printWriter.println("Hey, Tomcat");
+                printWriter.println("</title></head>");
+                printWriter.println("<body><h1>Hello Tomcat</h1></body>");
+                printWriter.println("</html>");
+            }
+        };
+
+        String servletName = "helloServlet";
+        tomcat.addServlet("/", servletName, servlet);
+        context.addServletMappingDecoded("/hello", servletName);
 
 //        HttpServlet
         tomcat.start();
-//        tomcat.getServer().await();;
+        tomcat.getServer().await();;
     }
 
 }
