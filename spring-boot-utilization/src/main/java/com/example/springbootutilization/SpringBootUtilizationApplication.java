aee14f18d847259e91f0ee2121d252c5cc07be3b
mins1031 <urisegea@naver.com>
2022-01-02 17:30:35 +0900

    스프링 부트 활용 첫 커밋

---
+++ spring-boot-utilization/src/main/java/com/example/springbootutilization/SpringBootUtilizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootutilization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUtilizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUtilizationApplication.class, args);
+    }
+
+}
